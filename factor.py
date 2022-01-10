6b8e96a21c97183d5624d85c3517522790c8759d
tonycloud40 <93011062+tonycloud40@users.noreply.github.com>
2022-01-10 18:06:52 -0500

    Fixed typos

--- factor.py
+++ factor.py
@@ -5,10 +5,10 @@ import sympy
 
 """
 The goal of this function is return the middle point to iterate 
-over a set of number to calculate the factors and Prime numbers and Composite numbers
+over a set of numbers to calculate the factors and Prime numbers and Composite numbers
 Once the quotient is smaller than the divisor then stop the process
 For smaller numbers no big deal but for huge numbers it will save a lot of time
-This is a limitation to the number of recursive level which around 952 levels
+This is a limitation to the number of recursive levels which around 952 levels
 """
 def getMiddle(dividend, d, previous, bottom):
     if dividend==1:
@@ -59,22 +59,21 @@ if __name__=='__main__':
     
     for nvs in range(2, n+1):
         nlist = np.array([])
-        #return a [[a,b],[m,n]]
+        #return array [[a,b],[m,n]]
         nl = getFactors(nvs)
 
         #create a new array [a,b,m,n]
         #convent to integer
         na = np.concatenate((nlist,np.array(nl)),axis=None).astype(int)
 
-        #eliminate repeatble values
+        #eliminate repeatable values
         nu = np.unique(na)
     
-        #convert to a ist
+        #convert to a list
         nm = (nu).tolist()
 
         #if you need test the results, uncomment the line
         #print(sympy.isprime(nvs))
-        #if sympy.isprime(nvs)==False: 
         print("#{} factors -> {} {}".format(nvs,\
                                     ','.join(str(e) for e in nm),\
                                     'Prime' if nu.size==2 else 'Composite'))
